93e1fcb6fa32e81c48d419466f6e4fef392be50f
Seoyeon Kang <z5404281@ad.unsw.edu.au>
2025-02-14 13:01:53 +1100

    Solved Leetcode 121. Best Time to Buy and Sell Stock

---
+++ bestTimetoBuyAndSellStock.py
@@ -0,0 +1,23 @@
+"""
+src : https://leetcode.com/problems/3sum/
+author : Seoyeon Kang
+date : 14/02/2025
+"""
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+        
+        return max_profit
+
