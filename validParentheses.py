6ae590b51f8e0680fc253732bdf87fdea578c4c3
Seoyeon Kang <z5404281@ad.unsw.edu.au>
2025-02-18 17:19:37 +1100

    Solved Leetcode 20. Valid Parentheses

---
+++ validParentheses.py
@@ -0,0 +1,24 @@
+"""
+src : https://leetcode.com/problems/valid-parentheses/
+author : Seoyeon Kang
+date : 18/02/2025
+"""
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
